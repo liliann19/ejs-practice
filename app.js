a83f332bcc15fa0acb5a7b3f304719db16b4455d
Lilian <210286008+liliann19@users.noreply.github.com>
2025-11-04 11:31:54 -0800

    added comments and created views folder

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ const app = express();
 
 const PORT = 3000; // Choose an available port number
 
+// sets the view engine
 app.set('view engine', 'ejs');
 
+// location where the webpage should look
 app.use(express.static('public'));
 
+// the default route 
 app.get('/', (req, res) => {
 
-  res.render('home');
+  res.render('home', { name: 'Donald Duck'});
 
 });
 
+// Start the server and listen on the specified port 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
